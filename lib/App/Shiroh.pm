fbeabb133598aa7fd59c4e36fa0aba4534bd5466
ytnobody <ytnobody@gmail.com>
2012-11-22 13:32:55 +0900

    init

--- lib/App/Shiroh.pm
+++ lib/App/Shiroh.pm
@@ -3,8 +3,32 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+use Net::Twitter::Lite;
+
+sub new {
+    my ($class, %opts) = @_;
+    my $message_to = delete $opts{message_to};
+    my $self = bless {opts => {%opts}, message_to => $message_to}, $class;
+    $self->init_client;
+    $self;
+}
+
+sub init_client {
+    my $self = shift;
+    $self->{client} = Net::Twitter::Lite->new( %{$self->{opts}} );
+}
+
+sub send {
+    my ($self, $mes) = @_;
+    my $client = $self->{client};
+    $mes = "d ". $self->{message_to}. " ". $mes;
+    $client->update($mes);
+}
+
 1;
 __END__
+
+=encoding utf-8
 
 =head1 NAME
 
@@ -12,11 +36,31 @@ App::Shiroh -
 
 =head1 SYNOPSIS
 
-  use App::Shiroh;
+In your shell, 
+
+  $ shiroh '俺がメッセージだ'
+
+or
+
+  $ echo '俺こそ真のメッセージだ' | shiroh
 
 =head1 DESCRIPTION
 
-App::Shiroh is
+App::Shiroh is a CLI posting tool for twitter
+
+=head1 CONFIG
+
+config-file is ~/.shiroh
+
+This is an example.
+
+  {
+     consumar_key        => 'YOUR CONSUMER KEY',
+     consumer_secret     => 'YOUR CONSUMER SECRET',
+     access_token        => 'YOUR ACCESS TOKEN',
+     access_token_secret => 'YOUR ACCESS TOKEN SECRET',
+     message_to          => 'some_nickname',
+  };
 
 =head1 AUTHOR
 
